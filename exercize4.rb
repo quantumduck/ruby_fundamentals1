fc9157163d7facfab201a7ba56a5eeb6c0cbb0a4
quantumduck <quantum.duck@hotmail.com>
2017-03-27 11:22:14 -0400

    variation on ex4

--- exercize4.rb
+++ exercize4.rb
@@ -1,15 +1,15 @@
 num_range = (1..100)
+ret_string = ""
 
 num_range.each do |num|
+  ret_string = num.to_s
   if (num % 3 == 0)
+    ret_string = "Bit"
     if (num % 5 == 0)
-      puts "BitMaker"
-    else
-      puts "Bit"
+      ret_string += "Maker"
     end
   elsif (num % 5 == 0)
-    puts "Maker"
-  else
-    puts num
+    ret_string = "Maker"
   end
+  puts ret_string
 end
